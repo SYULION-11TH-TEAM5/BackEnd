e6f355bcc5f1f2ec40a638f32c6dd5234d2ceb67
seungbin-park <tmdqls0212@naver.com>
2023-08-18 02:09:05 +0900

    Feat: SignUp & Login

--- src/main/java/com/likelion11/demo/repository/MemberRepository.java
+++ src/main/java/com/likelion11/demo/repository/MemberRepository.java
@@ -1,7 +1,14 @@
 package com.likelion11.demo.repository;
 
+import com.likelion11.demo.dto.memberDto;
 import com.likelion11.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByUsername(String username);
+
+    Member findByEmail(String email);
+
+    //void insertNewUser()
 }
